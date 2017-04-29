abf02e203ad11bdb98db77c32c8acd8e31fbaf64
Vincent Lejeune <vljn.ovi@gmail.com>
2017-04-29 23:36:18 +0200

    Do not crash if trying to change profile picture but cancelling

--- client/MainPage.xaml.cpp
+++ client/MainPage.xaml.cpp
@@ -79,26 +79,34 @@ void client::MainPage::AppBarButton_Click_2(Platform::Object^ sender, Windows::U
 	openPicker->FileTypeFilter->Append(".png");
 
 	Concurrency::create_task(openPicker->PickSingleFileAsync()).then([](Windows::Storage::StorageFile^ file)
-	{
-		return Windows::Storage::FileIO::ReadBufferAsync(file);
-	}).then([](Windows::Storage::Streams::IBuffer^ buffer)
-	{
-		auto encoded_buffer = Windows::Security::Cryptography::CryptographicBuffer::EncodeToBase64String(buffer);
-		auto localSettings = Windows::Storage::ApplicationData::Current->LocalSettings;
+		{
+			if (file == nullptr)
+				throw std::exception();
+			return Windows::Storage::FileIO::ReadBufferAsync(file);
+		}).then([](Concurrency::task<Windows::Storage::Streams::IBuffer^> bufferOp)
+		{
+			try
+			{
+				auto buffer = bufferOp.get();
+				auto encoded_buffer = Windows::Security::Cryptography::CryptographicBuffer::EncodeToBase64String(buffer);
+				auto localSettings = Windows::Storage::ApplicationData::Current->LocalSettings;
 
-		auto upload_pic = ref new Windows::Web::Http::HttpMultipartFormDataContent();
-		upload_pic->Add(ref new Windows::Web::Http::HttpStringContent("data:image/png;base64," + encoded_buffer), "avatar");
-		upload_pic->Add(ref new Windows::Web::Http::HttpStringContent(
-			dynamic_cast<String^>(localSettings->Values->Lookup("access_token"))), "access_token");
+				auto upload_pic = ref new Windows::Web::Http::HttpMultipartFormDataContent();
+				upload_pic->Add(ref new Windows::Web::Http::HttpStringContent("data:image/png;base64," + encoded_buffer), "avatar");
+				upload_pic->Add(ref new Windows::Web::Http::HttpStringContent(
+					dynamic_cast<String^>(localSettings->Values->Lookup("access_token"))), "access_token");
 
-		auto client = ref new Windows::Web::Http::HttpClient();
-		auto request = ref new Windows::Web::Http::HttpRequestMessage(
-			ref new Windows::Web::Http::HttpMethod("PATCH"),
-			ref new Windows::Foundation::Uri("https://oc.todon.fr/api/v1/accounts/update_credentials"));
-		request->Content = upload_pic;
-		client->SendRequestAsync(request);
-		return;
-	});
+				auto client = ref new Windows::Web::Http::HttpClient();
+				auto request = ref new Windows::Web::Http::HttpRequestMessage(
+					ref new Windows::Web::Http::HttpMethod("PATCH"),
+					ref new Windows::Foundation::Uri("https://oc.todon.fr/api/v1/accounts/update_credentials"));
+				request->Content = upload_pic;
+				client->SendRequestAsync(request);
+			}
+			catch (...)
+			{
+			}
+		});
 }
 
 void client::MainPage::onBack(Platform::Object ^ sender, Windows::UI::Core::BackRequestedEventArgs ^ e)
