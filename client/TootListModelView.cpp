45da4141297bbde47af63d9fb65f268ec14512b3
vlj <vljn.ovi@gmail.com>
2017-05-10 22:00:41 +0200

    Make notifications more informative and only show the ones added after last usage.

--- client/TootListModelView.cpp
+++ client/TootListModelView.cpp
@@ -7,7 +7,7 @@ client::TootListModelView::TootListModelView()
 {
 	_timeline = ref new Collections::Vector<Toot^>();
 	_notifications = ref new Collections::Vector<Notification^>();
-	refresh();
+//	refresh();
 }
 
 void client::TootListModelView::refresh()
@@ -39,26 +39,65 @@ void client::TootListModelView::refresh()
 	{
 		auto refreshednotifications = ref new Platform::Collections::Vector<Notification^>();
 		auto toastNotifier = Windows::UI::Notifications::ToastNotificationManager::CreateToastNotifier();
+		auto localSettings = Windows::Storage::ApplicationData::Current->LocalSettings;
+		auto ptrvalue = localSettings->Values->Lookup("last_notification");
+		const auto& lastId = ptrvalue == nullptr ? 0 : (unsigned int)ptrvalue;
+		auto newLastId = lastId;
 		for (const auto& n : v)
 		{
 			refreshednotifications->Append(ref new Notification(n));
 
-			auto toastVisual =
+			if (n.id <= lastId)
+				continue;
+			newLastId = std::max<unsigned int>(newLastId, n.id);
+			const auto& removeHtml = [](const auto& content)
+			{
+				return std::wstring{
+					Windows::Data::Html::HtmlUtilities::ConvertToText(
+					ref new Platform::String(content.data())
+				)->Data() };
+			};
+
+			std::wstring&& toastVisual =
 				LR"(<toast launch="app-defined-string"><visual>
-		<binding template='ToastGeneric'>
-		<text>Ducktodon</text>
-		<text>)" + n.account.display_name + LR"(</text>
-		</binding>
-		</visual></toast>)";
+				<binding template='ToastGeneric'>
+				<text>Ducktodon</text>
+				<text>)";
+			if (n.type == Mastodon::NotificationType::favourite)
+			{
+				toastVisual += n.account.display_name + LR"( has favorited your toot :</text>)";
+				toastVisual += LR"(<text>)" + removeHtml(n.status->content) + LR"(</text>)";
+			}
+			else if (n.type == Mastodon::NotificationType::reblog)
+			{
+				toastVisual += n.account.display_name + LR"( has reblogged your toot :</text>)";
+				toastVisual += LR"(<text>)" + removeHtml(n.status->content) + LR"(</text>)";
+			}
+			else if (n.type == Mastodon::NotificationType::mention)
+			{
+				toastVisual += n.account.display_name + LR"( has mentioned you :</text>)";
+				toastVisual += LR"(<text>)" + removeHtml(n.status->content) + LR"(</text>)";
+			}
+			else if (n.type == Mastodon::NotificationType::follow)
+			{
+				toastVisual += n.account.display_name + LR"( has followed you</text>)";
+			}
+			toastVisual += LR"(</binding>
+				</visual></toast>)";
 			auto xml = ref new Windows::Data::Xml::Dom::XmlDocument();
 			xml->LoadXml(ref new Platform::String(toastVisual.data()));
 			auto notif = ref new Windows::UI::Notifications::ToastNotification(xml);
 			toastNotifier->Show(notif);
 		}
-		_notifications = refreshednotifications;
 		Windows::ApplicationModel::Core::CoreApplication::MainView->CoreWindow->Dispatcher->RunAsync(
 			Windows::UI::Core::CoreDispatcherPriority::Low,
-			ref new Windows::UI::Core::DispatchedHandler([this]() {
+			ref new Windows::UI::Core::DispatchedHandler([this, lastId, newLastId, refreshednotifications]() {
+			if (lastId != newLastId)
+			{
+				auto localSettings = Windows::Storage::ApplicationData::Current->LocalSettings;
+				const auto& lastId = localSettings->Values->Insert("last_notification", newLastId);
+			}
+			_notifications = refreshednotifications;
 			PropertyChanged(this, ref new Windows::UI::Xaml::Data::PropertyChangedEventArgs("Notifications"));
 		}));
 	});
