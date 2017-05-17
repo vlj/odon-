62272438f2ecfc5fe4625e2ebc0a30149d3c465c
vlj <vljn.ovi@gmail.com>
2017-05-17 21:53:20 +0200

    Tweak Settings page visual

--- client/MainPage.xaml.cpp
+++ client/MainPage.xaml.cpp
@@ -10,6 +10,7 @@
 #include "Timeline.xaml.h"
 #include "Search.xaml.h"
 #include "TootListModelView.h"
+#include "Settings.xaml.h"
 
 using namespace client;
 
@@ -55,42 +56,7 @@ void client::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml:
 
 void client::MainPage::AppBarButton_Click_2(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
-	Windows::Storage::Pickers::FileOpenPicker^ openPicker = ref new Windows::Storage::Pickers::FileOpenPicker();
-	openPicker->ViewMode = Windows::Storage::Pickers::PickerViewMode::Thumbnail;
-	openPicker->SuggestedStartLocation = Windows::Storage::Pickers::PickerLocationId::PicturesLibrary;
-	openPicker->FileTypeFilter->Append(".jpg");
-	openPicker->FileTypeFilter->Append(".jpeg");
-	openPicker->FileTypeFilter->Append(".png");
-
-	Concurrency::create_task(openPicker->PickSingleFileAsync()).then([](Windows::Storage::StorageFile^ file)
-		{
-			if (file == nullptr)
-				throw std::exception();
-			return Windows::Storage::FileIO::ReadBufferAsync(file);
-		}).then([](Concurrency::task<Windows::Storage::Streams::IBuffer^> bufferOp)
-		{
-			try
-			{
-				auto buffer = bufferOp.get();
-				auto encoded_buffer = Windows::Security::Cryptography::CryptographicBuffer::EncodeToBase64String(buffer);
-				auto localSettings = Windows::Storage::ApplicationData::Current->LocalSettings;
-
-				auto upload_pic = ref new Windows::Web::Http::HttpMultipartFormDataContent();
-				upload_pic->Add(ref new Windows::Web::Http::HttpStringContent("data:image/png;base64," + encoded_buffer), "avatar");
-				upload_pic->Add(ref new Windows::Web::Http::HttpStringContent(
-					dynamic_cast<String^>(localSettings->Values->Lookup("access_token"))), "access_token");
-
-				auto client = ref new Windows::Web::Http::HttpClient();
-				auto request = ref new Windows::Web::Http::HttpRequestMessage(
-					ref new Windows::Web::Http::HttpMethod("PATCH"),
-					ref new Windows::Foundation::Uri("https://oc.todon.fr/api/v1/accounts/update_credentials"));
-				request->Content = upload_pic;
-				client->SendRequestAsync(request);
-			}
-			catch (...)
-			{
-			}
-		});
+	contentFrame->Navigate(Windows::UI::Xaml::Interop::TypeName(Settings::typeid), nullptr);
 }
 
 void client::MainPage::onBack(Platform::Object ^ sender, Windows::UI::Core::BackRequestedEventArgs ^ e)
--- client/Settings.xaml.cpp
+++ client/Settings.xaml.cpp
@@ -25,3 +25,42 @@ Settings::Settings()
 {
 	InitializeComponent();
 }
+
+/*	Windows::Storage::Pickers::FileOpenPicker^ openPicker = ref new Windows::Storage::Pickers::FileOpenPicker();
+openPicker->ViewMode = Windows::Storage::Pickers::PickerViewMode::Thumbnail;
+openPicker->SuggestedStartLocation = Windows::Storage::Pickers::PickerLocationId::PicturesLibrary;
+openPicker->FileTypeFilter->Append(".jpg");
+openPicker->FileTypeFilter->Append(".jpeg");
+openPicker->FileTypeFilter->Append(".png");
+
+Concurrency::create_task(openPicker->PickSingleFileAsync()).then([](Windows::Storage::StorageFile^ file)
+{
+if (file == nullptr)
+throw std::exception();
+return Windows::Storage::FileIO::ReadBufferAsync(file);
+}).then([](Concurrency::task<Windows::Storage::Streams::IBuffer^> bufferOp)
+{
+try
+{
+auto buffer = bufferOp.get();
+auto encoded_buffer = Windows::Security::Cryptography::CryptographicBuffer::EncodeToBase64String(buffer);
+auto localSettings = Windows::Storage::ApplicationData::Current->LocalSettings;
+
+auto upload_pic = ref new Windows::Web::Http::HttpMultipartFormDataContent();
+upload_pic->Add(ref new Windows::Web::Http::HttpStringContent("data:image/png;base64," + encoded_buffer), "avatar");
+upload_pic->Add(ref new Windows::Web::Http::HttpStringContent(
+dynamic_cast<String^>(localSettings->Values->Lookup("access_token"))), "access_token");
+
+auto client = ref new Windows::Web::Http::HttpClient();
+auto request = ref new Windows::Web::Http::HttpRequestMessage(
+ref new Windows::Web::Http::HttpMethod("PATCH"),
+ref new Windows::Foundation::Uri("https://oc.todon.fr/api/v1/accounts/update_credentials"));
+request->Content = upload_pic;
+client->SendRequestAsync(request);
+}
+catch (...)
+{
+}
+});*/
+
+
